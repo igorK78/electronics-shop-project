f38b3d5fdc43e74e49207a543490e3322f9d86d6
IgorK78 <i.kolegov78@gmail.com>
2023-06-01 19:31:22 +0300

    edit IgorK

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item3 = Item('Кабель', 10, 5)
+
+
+def test_item_init():
+    assert item3.name == "Кабель"
+    assert item3.price == 10
+    assert item3.quantity == 5
+
+
+def test_calculate_total_price():
+    assert item3.calculate_total_price() == 50
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.8
+    item3.apply_discount()
+    assert item3.price == 8
