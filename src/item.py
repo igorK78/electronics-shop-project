e6f98549324f230ca9489a50331f4a79558f642b
IgorK78 <i.kolegov78@gmail.com>
2023-06-04 23:56:49 +0300

    edit from homework 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,19 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) <= 10:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -24,11 +36,46 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity * self.pay_rate
+        return self.price * self.quantity
 
-    def apply_discount(self) -> float:
+    def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
-        return self.pay_rate
+
+    @staticmethod
+    def string_to_number(value: str):
+        return int(value)
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        """
+        класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+        """
+        try:
+            with open(filename) as f:
+                reader = csv.DictReader(f)
+                cls.all.clear()
+                for row in reader:
+                    name = row['name']
+                    price = cls.string_to_number(row['price'])
+                    quantity = cls.string_to_number(row['quantity'])
+                    cls.all.append(cls(name, price, quantity))  # создаем экзмляры классы и кладем их в список
+        except FileNotFoundError:
+            return f"Отсутствует файл {filename}"
+        except InstantiateCSVError:
+            return f"Файл {filename} поврежден"
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.name
+
+
+class InstantiateCSVError(BaseException):
+    pass
+
+
+Item.instantiate_from_csv("C:\electronics-shop-project\src\items.csv")
