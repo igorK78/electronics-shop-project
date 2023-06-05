572df3efd68d757ad83e34ec3fbc04de315adcae
igor <i.kolegov78@gmail.com>
2023-06-05 20:32:50 +0300

    edit from hw-2

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,23 +3,27 @@ import pytest
 
 from src.item import Item
 
+
 @pytest.fixture
 def instance_item():
-    return Item('Кабель', 10, 5)
+    return Item("Смартфон", 10000, 20)
+
 
 def test_calculate_total_price(instance_item):
-    assert instance_item.calculate_total_price() == 50
+    assert instance_item.calculate_total_price() == 200000
 
 
 def test_apply_discount(instance_item):
     instance_item.pay_rate = 0.8
     instance_item.apply_discount()
-    assert instance_item.price == 8
+    assert instance_item.price == 8000.0
+
 
 def test_name(instance_item):
-    assert instance_item.name == 'Кабель'
-    instance_item.name = 'СуперКабель'
+    assert instance_item.name == 'Смартфон'
+    instance_item.name = 'СуперСмартфон'
     assert instance_item.name == 'Exception: Длина наименования товара превышает 10 символов.'
+
 
 def test_string_to_number(instance_item):
     assert instance_item.string_to_number('123') == 123
